eb136697e66e7d5fec528a5188490f79266ca31c
Ezekiel Sebastine <SEzekiel@users.noreply.github.com>
2017-02-25 22:23:46 +0000

    Added a header.php file to be included in all pages in order to avoid typing nas all the time

---
+++ Project/header.php
@@ -0,0 +1,25 @@
+    <ul id="dropdown1" class="dropdown-content" style="width: 200px">
+      <li><a href="">Add patient</a></li>
+      <li><a href="users.php">View registered patients</a></li>
+      <li><a href="#!">Find a patient</a></li>
+      <li class="divider"></li>
+      <li><a href="#!">three</a></li>
+    </ul>
+     <div class="navbar-fixed"> 
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo right">Logo</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="dashboard.php">Home</a></li>
+        <li><a class="dropdown-button" href="#!" data-activates="dropdown1" data-constrainWidth="false">Patient<i class="material-icons right"><span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span></i></a></li>
+        <li><a href="collapsible.html">Something</a></li>
+      </ul>
+      <ul id="slide-out" class="side-nav">
+        <li><a href="dashboard.php">Home</a></li>
+        <li><a class="dropdown-button" href="#!" data-activates="dropdown1" data-constrainWidth="false">Patient<i class="material-icons right"><span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span></i></a></li>
+        <li><a href="collapsible.html">Something</a></li>
+      </ul>
+      <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">Menu</i></a>
+    </div>
+  </nav>
+  </div>
