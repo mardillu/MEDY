fcde811645fde3688721b8fd64c135b83a0615e2
Ezekiel Sebastine <SEzekiel@users.noreply.github.com>
2017-02-26 23:35:10 +0000

    Updated header with logo
    Added logo to nav

--- Project/header2.php
+++ Project/header2.php
@@ -1,7 +1,7 @@
      <div class="navbar-fixed"> 
   <nav>
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo right">Logo</a>
+            <img src="images/logo.PNG" class="brand-logo right" height="120" width="120" alt="Logo" style="margin-top: -30px"></a>
     </div>
   </nav>
   </div>
