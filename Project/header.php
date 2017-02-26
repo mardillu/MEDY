87985a6bb4fd457c73171bb8ee0707e33a2b7261
Ezekiel Sebastine <SEzekiel@users.noreply.github.com>
2017-02-26 22:48:40 +0000

    Updated header
    Updated header with search field

--- Project/header.php
+++ Project/header.php
@@ -1,23 +1,38 @@
-    <ul id="dropdown1" class="dropdown-content" style="width: 200px">
-      <li><a href="">Add patient</a></li>
-      <li><a href="users.php">View registered patients</a></li>
-      <li><a href="#!">Find a patient</a></li>
-      <li class="divider"></li>
-      <li><a href="#!">three</a></li>
-    </ul>
      <div class="navbar-fixed"> 
   <nav>
     <div class="nav-wrapper">
       <a href="#" class="brand-logo right">Logo</a>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
         <li><a href="dashboard.php">Home</a></li>
-        <li><a class="dropdown-button" href="#!" data-activates="dropdown1" data-constrainWidth="false">Patient<i class="material-icons right"><span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span></i></a></li>
-        <li><a href="collapsible.html">Something</a></li>
+        <li><a href="">Add patient</a></li>
+        <li><a href="users.php">View registered patients</a></li>
+		<li>
+			<div class="row" >
+			    <form class="col s12">
+			      <div class="row">
+			        <div class="input-field col s12" style="background-color: white;color: black">
+			          <input id="text" type="text" class="validate">
+			          <label for="text" data-error="wrong" data-success="right">Find a patient... </label>
+			          <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+			        </div>
+			      </div>
+			    </form>
+			 </div>
+  </li>
       </ul>
       <ul id="slide-out" class="side-nav">
         <li><a href="dashboard.php">Home</a></li>
         <li><a class="dropdown-button" href="#!" data-activates="dropdown1" data-constrainWidth="false">Patient<i class="material-icons right"><span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span></i></a></li>
         <li><a href="collapsible.html">Something</a></li>
+        <li>
+	      <form>
+	        <div class="input-field">
+	          <input id="search" type="search" required>
+	          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+	          <i class="material-icons">close</i>
+	        </div>
+	      </form>
+        </li>
       </ul>
       <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">Menu</i></a>
     </div>
