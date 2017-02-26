ca8497c3ce022cab92e50ca499c21d9a25594696
Ezekiel Sebastine <SEzekiel@users.noreply.github.com>
2017-02-26 17:52:29 +0000

    Login, index, page with updates header, footer etc

--- Project/index.php
+++ Project/index.php
@@ -6,13 +6,17 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Login-MEDY</title>
-
+         <!-- Materialize css. Compiled and minified CSS -->
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
 		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
-		<div id="login" style="width: 50%; margin-left: 25%; margin-top: 10%">
+	    <?php
+      include "header2.php";
+    ?>
+		<div id="login" style="width: 50%; margin-left: 25%; margin-top: 5%">
 		<form action="admin/dashboard.php" method="post" accept-charset="utf-8">
             <table class="table table-bordered">
                 <thead><h2>Login in seconds</h2></thead>
@@ -29,11 +33,15 @@
             		<tr>
             			<td> <input type="submit" name="submit" value="Login" class="btn btn-success" style="width: 100%;"></td>
             		</tr>
+            		<tr>
+            			<td> 
+            				<a href="hregister.php" class="btn btn-default white" style="width: 100%; color:blue">Register a hospital</a>
+            			</td>
+            		</tr>
             	</tbody>
             </table>
             </form>
 		</div>
-
 		<!-- jQuery -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<!-- Bootstrap JavaScript -->
