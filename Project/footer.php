07ac2c481a7395f73c4e910eecabf32cb7ed47b8
Ezekiel Sebastine <SEzekiel@users.noreply.github.com>
2017-02-26 17:50:24 +0000

    Project footer
    project footer to be included in all pages

---
+++ Project/footer.php
@@ -0,0 +1,25 @@
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">About MEDY</h5>
+                <p class="grey-text text-lighten-4">MEDY is a cloud based system for storing patient health record. It offers flexibility in accessing patients health data and past medical records. It does this by seamlessly storing patients health information and medical record in a central database. With this functionality, MEDY can be used to track outbreak of diseases.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Developers</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="https://ezekielsebastinecv.wordpress.com/">Ezekiel Sebastine</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Ruth Karimi</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Nii Quartey</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            <div class="container">
+            © <?php echo date("Y")?> Alrights reserved
+            <a class="grey-text text-lighten-4 right" href="#!">MEDY</a>
+            </div>
+          </div>
+        </footer>
